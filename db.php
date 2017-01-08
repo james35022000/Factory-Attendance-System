7254ef52fb37bc675e55c2814be3f9bbf7d4581d
JCChen <james35022000@gmail.com>
2017-01-08 19:13:50 +0800

    Remove informations

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 
 	<?php
 		error_reporting(0);
-		$conn = mysql_connect("localhost","canwall","_8!FvVaP4L7K");
-		$db   = mysql_select_db('canwall_test');
+		$conn = mysql_connect("localhost","","");
+		$db   = mysql_select_db('');
 		//   mysqli_query($conn,  "SET collation_connection = ‘utf8_general_ci`");
 		//	var_dump($conn);
 		//	var_dump($db);
